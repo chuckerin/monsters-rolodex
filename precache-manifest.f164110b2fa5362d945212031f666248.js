ee7e2b0bd1550b3b49382bcd4ae8d248da167b9d
Chuck Black <CBlack@ADEV.LOCAL>
2020-04-16 22:04:11 -0500

    Updates

---
+++ precache-manifest.f164110b2fa5362d945212031f666248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babe667f2e20515b396cb4c325ab802b",
+    "url": "/monsters-rolodex/index.html/index.html"
+  },
+  {
+    "revision": "315582c86668497d424c",
+    "url": "/monsters-rolodex/index.html/static/css/main.2d9aea77.chunk.css"
+  },
+  {
+    "revision": "0f558170f71217fe2a40",
+    "url": "/monsters-rolodex/index.html/static/js/2.7b1bd289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/index.html/static/js/2.7b1bd289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315582c86668497d424c",
+    "url": "/monsters-rolodex/index.html/static/js/main.1f84c0ca.chunk.js"
+  },
+  {
+    "revision": "0eb2304d8c2bb20bb9ca",
+    "url": "/monsters-rolodex/index.html/static/js/runtime-main.0aedd4a0.js"
+  }
+]);
